355f472b79df364dabc367e767c19428cd8c4ef2
Hannu Kujanpää <61244601+hjkuja@users.noreply.github.com>
2026-01-28 20:42:06 +0200

    Init project

---
+++ src/pages/Courses.tsx
@@ -0,0 +1,12 @@
+export default function Courses() {
+    return (
+        <div>
+            <h1>
+                Friba Courses
+            </h1>
+            <p>
+                Here are the friba courses.
+            </p>
+        </div>
+    );
+}
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,12 @@
+export default function HomePage() {
+    return (
+        <div>
+            <h1>
+                FribaScore
+            </h1>
+            <p>
+                This is the FribaScore app.
+            </p>
+        </div>
+    );
+}
