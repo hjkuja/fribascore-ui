dc28d05e7134ac602fa964cbca8427bdbe8aae65
Hannu Kujanpää <61244601+hjkuja@users.noreply.github.com>
2026-01-29 20:59:09 +0200

    Add configurable host name

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,24 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react({
-    babel: {
-      plugins: [["babel-plugin-react-compiler"]]
-    }
-  })],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  const extraHost = env.ALLOWED_HOST || env.VITE_ALLOWED_HOST
+
+  return {
+    plugins: [react({
+      babel: {
+        plugins: [["babel-plugin-react-compiler"]]
+      }
+    })],
+    server: {
+      allowedHosts: [
+        'localhost',
+        '127.0.0.1',
+        '0.0.0.0',
+        ...(extraHost ? [extraHost] : []),
+      ].filter(Boolean), // filter out empty strings
+    },
+  }
 })
