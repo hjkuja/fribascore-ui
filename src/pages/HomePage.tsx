9a42f934a4c849d626e033b589594ca7ac1426e7
Hannu Kujanpää <61244601+hjkuja@users.noreply.github.com>
2026-02-02 21:46:48 +0200

    Update welcome page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,12 +1,7 @@
+import Welcome from "../components/Welcome/Welcome";
+
 export default function HomePage() {
     return (
-        <div>
-            <h1>
-                FribaScore
-            </h1>
-            <p>
-                This is the FribaScore app.
-            </p>
-        </div>
+        <Welcome />
     );
 }
